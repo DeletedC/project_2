aa9a2c5a762e78595ca9eabd4c45e186590a75d9
Curtis <CurtisWoodsArt@gmail.com>
2020-05-24 16:59:27 -0400

    Started Bootstrap styling on Index.jsx

--- views/Index.jsx
+++ views/Index.jsx
@@ -6,22 +6,26 @@ class Index extends React.Component {
         const {students} = this.props;
         return (
             <Layout>
-                <h1>Hellooooooo! I'm an index!</h1>
-                <nav>
-                    <a href="/students">Home</a>
-                    <a href="/students/new">New Student</a>
-                </nav>
-                <div>
-                    {students.map((student, index) => {
-                        return (
-                            <>
-                            <a href={`/students/${student._id}`}>
-                                <h2>{student.nameFirst} {student.nameLast}</h2>
-                            </a>
-                            <a href={`/students/edit/${student._id}`}>Edit</a>
-                            </>
-                        );
-                    })}
+                <div className="container-fluid">
+                    <div className="jumbotron">
+                        <h1>Student Tracker</h1>
+                    </div>
+                    <nav className="navbar justify-content-end">
+                        <a className="nav-link" href="/students">Home</a>
+                        <a className="nav-link" href="/students/new">New Student</a>
+                    </nav>
+                    <div>
+                        {students.map((student, index) => {
+                            return (
+                                <>
+                                <a href={`/students/${student._id}`}>
+                                    <h2>{student.nameFirst} {student.nameLast}</h2>
+                                </a>
+                                <a href={`/students/edit/${student._id}`}>Edit</a>
+                                </>
+                            );
+                        })}
+                    </div>
                 </div>
             </Layout>
         );
