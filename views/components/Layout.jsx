a4ac492fa61fb9d91f4ac1e16809c72266376697
DeletedC <CurtisWoodsArt@gmail.com>
2020-11-23 12:08:01 -0500

    Fixed crossorigin camel case error

--- views/components/Layout.jsx
+++ views/components/Layout.jsx
@@ -6,7 +6,7 @@ class Layout extends React.Component {
             <html>
                 <head>
                     {/* Bootstrap */}
-                    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous"/>
+                    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossOrigin="anonymous"/>
                     
                     {/* Personal CSS */}
                     <link href="/css/style.css" rel="stylesheet"/>
