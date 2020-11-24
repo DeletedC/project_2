4a18e313a67ec17df727dbdd5812f6e753ec3d31
DeletedC <CurtisWoodsArt@gmail.com>
2020-11-24 12:14:29 -0500

    Added database connection checker and conditional rendering

--- views/Index.jsx
+++ views/Index.jsx
@@ -4,17 +4,29 @@ const Layout = require('./components/Layout.jsx');
 class Index extends React.Component {
     render() {
         const {students} = this.props;
+        const isDatabaseConnected = () => {
+            if (students == 'noDatabase') {
+                console.log(students);
+                return false;
+            } else {
+                console.log(students);
+                return true;
+            }
+        }
 
         // Alphabetizing the array
-        students.sort((a, b) => {
-            if (a.nameLast.toUpperCase() > b.nameLast.toUpperCase()) {
-                return 1;
-            } else if (a.nameLast.toUpperCase() < b.nameLast.toUpperCase()) {
-                return -1;
-            } else {
-                return 0;
-            }
-        });
+        if (isDatabaseConnected()) {
+            students.sort((a, b) => {
+                if (a.nameLast.toUpperCase() > b.nameLast.toUpperCase()) {
+                    return 1;
+                } else if (a.nameLast.toUpperCase() < b.nameLast.toUpperCase()) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            });
+        }
+        
 
         return (
             <Layout>
@@ -26,24 +38,31 @@ class Index extends React.Component {
                     </div>
                     <nav className="nav justify-content-center">
                         <a className="nav-item nav-link" href="/students" role="button">Home</a>
-                        <a className="nav-item nav-link" href="/students/new" role="button">New Student</a>
+                        {isDatabaseConnected()
+                            ? <a className="nav-item nav-link" href="/students/new" role="button">New Student</a>
+                            : ''
+                        }
                     </nav>
                     <div className="col-8">
-                        {students.map((student, index) => {
-                            return (
-                                <>
-                                <a href={`/students/${student._id}`}>
-                                    <h2>{student.nameFirst} {student.nameLast}</h2>
-                                </a>
-                                <form action={`/students/edit/${student._id}`} method="get">
-                                    <input className="btn btn-outline-secondary" type="submit" value="Edit"/>
-                                </form>
-                                <form action={`/students/${student._id}?_method=DELETE`} method="post">
-                                    <input className="btn btn-outline-danger" type="submit" value="Delete"/>
-                                </form>
-                                </>
-                            );
-                        })}
+                        {isDatabaseConnected()
+                            ? students.map((student, index) => {
+                                return (
+                                    <>
+                                    <a href={`/students/${student._id}`}>
+                                        <h2>{student.nameFirst} {student.nameLast}</h2>
+                                    </a>
+                                    <form action={`/students/edit/${student._id}`} method="get">
+                                        <input className="btn btn-outline-secondary" type="submit" value="Edit"/>
+                                    </form>
+                                    <form action={`/students/${student._id}?_method=DELETE`} method="post">
+                                        <input className="btn btn-outline-danger" type="submit" value="Delete"/>
+                                    </form>
+                                    </>
+                                );
+                            })
+
+                            : <p>The database is not connected. Please try again later.</p>
+                        }
                     </div>
                 </div>
             </Layout>
